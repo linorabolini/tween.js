2341e3ce6255d2a5792093b974f89a1043b2a77a
Lino Rabolini <lino.rabolini@consultant.lego.com>
2026-04-02 00:08:17 +0200

    chore: some reverts

--- src/Tween.ts
+++ src/Tween.ts
@@ -113,7 +113,8 @@ export class Tween<T extends UnknownProps = any> {
 	}
 
 	to(target: UnknownProps, duration?: number): this {
-		if (this._isPlaying) this.stop()
+		if (this._isPlaying)
+			throw new Error('Can not call Tween.to() while Tween is already started or paused. Stop the Tween first.')
 
 		this._valuesEnd = target
 		this._propertiesAreSetUp = false
